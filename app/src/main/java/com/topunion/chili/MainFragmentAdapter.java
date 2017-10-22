37eea8491fff93c6ec8e8ea901f4bc6e1af43302
renxiaoming <renxiaoming@caiyitech.com>
2017-10-22 11:53:28 +0800

    update

--- app/src/main/java/com/topunion/chili/MainFragmentAdapter.java
+++ app/src/main/java/com/topunion/chili/MainFragmentAdapter.java
@@ -3,6 +3,8 @@ package com.topunion.chili;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.view.ViewGroup;
 
 import java.util.List;
 
@@ -14,10 +16,10 @@ public class MainFragmentAdapter extends FragmentPagerAdapter {
     private FragmentManager mFragmetnManager;
     private List<Fragment> mListfragment;
 
-    public MainFragmentAdapter(FragmentManager fm,List<Fragment> list) {
+    public MainFragmentAdapter(FragmentManager fm, List<Fragment> list) {
         super(fm);
-        this.mFragmetnManager=fm;
-        this.mListfragment=list;
+        this.mFragmetnManager = fm;
+        this.mListfragment = list;
     }
 
     @Override
@@ -28,5 +30,11 @@ public class MainFragmentAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return mListfragment.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment f = (Fragment) super.instantiateItem(container, position);
+        return f;
     }
 }
